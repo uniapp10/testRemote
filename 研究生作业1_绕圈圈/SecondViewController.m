98067985a43bf4fedcb10e684504e57a77dace39
uniapp10 <zhudong10@163.com>
2018-06-20 10:46:05 +0800

    testXcodeServer 03

--- 研究生作业1_绕圈圈/SecondViewController.m
+++ 研究生作业1_绕圈圈/SecondViewController.m
@@ -14,14 +14,38 @@
 
 @implementation SecondViewController
 
+#define ScreenSize [UIScreen mainScreen].bounds.size
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor brownColor];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    CGFloat btnW = 100;
+    CGFloat leftMargin = 30;
+    CGFloat centerMargin  = ScreenSize.width - 2 * (btnW + leftMargin);
+    btn.frame = CGRectMake(leftMargin, 100, btnW, 40);
+    btn.backgroundColor = [UIColor greenColor];
+    [btn setTitle:@"showChart" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(show) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    UIButton *btn_close = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn_close.frame = CGRectMake(leftMargin + btnW + centerMargin, 100, btnW, 40);
+    btn_close.backgroundColor = [UIColor redColor];
+    [btn_close setTitle:@"closeChart" forState:UIControlStateNormal];
+    [btn_close addTarget:self action:@selector(close) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn_close];
+    
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true) firstObject];
+    NSString *finalPath = [path stringByAppendingPathComponent:@"log/Test_20170711.xlog"];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)show {
+    
+}
+
+- (void)close {
+    
 }
 
 /*
